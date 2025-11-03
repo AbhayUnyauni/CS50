14463998b69e57f96a2a77ae5accdb9bce5c85d8
Abhay Unyauni <abhayunyauni@gmail.com>
2025-11-03 11:29:36 +0530

    Add Mario pyramid program in mario.c

---
+++ Mario-Less/mario.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+   int height;
+    do
+    {
+        height = get_int("height: ");
+    }
+    while (height <= 0);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
