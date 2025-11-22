04c47b68582e9039e58eb03b7ce0d75d3cb75b9e
Abhay Unyauni <abhayunyauni@gmail.com>
2025-11-22 11:59:46 +0530

    Implement linked list insertion at start
    This program creates a linked list by inserting nodes at the start based on user input.

---
+++ Single_linked_list_programs/insertStart.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+}Node;
+
+Node* createNode(int data){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(newNode == NULL){
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main(){
+    int n, value;
+    Node* head = NULL;
+    Node* temp = NULL;
+
+    n = get_int("Enter the value of nodes: ");
+    if(n <= 0){
+        printf("Invalid number of Nodes!\n");
+        return 1;
+    }
+    for(int i = 0; i < n; i++){
+        value = get_int("Enter the value for each node: ");
+        Node* newNode = createNode(value);
+        if(head == NULL){
+            head = newNode;
+            temp = newNode;
+        }else{
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    printf("Linked ;List: ");
+    temp = head;
+    while(temp != NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+    return 0;
+}
